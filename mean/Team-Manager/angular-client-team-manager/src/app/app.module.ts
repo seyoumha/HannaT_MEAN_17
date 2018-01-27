a7c248458f1535f59f980b74735d7a65b967978c
seyoumha <seyoumha@gmail.com>
2018-01-27 14:16:13 -0500

    everything else

---
+++ mean/Team-Manager/angular-client-team-manager/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {PlayerService} from './player.service'
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { ListPlayersComponent } from './list-players/list-players.component';
+import { AddPlayerComponent } from './add-player/add-player.component';
+import { PlayerStatusComponent } from './player-status/player-status.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { PlayerStatus1Component } from './player-status1/player-status1.component';
+import { PlayerStatus2Component } from './player-status2/player-status2.component';
+import { PlayerStatus3Component } from './player-status3/player-status3.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListPlayersComponent,
+    AddPlayerComponent,
+    PlayerStatusComponent,
+    PlayerStatus1Component,
+    PlayerStatus2Component,
+    PlayerStatus3Component,
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [PlayerService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
